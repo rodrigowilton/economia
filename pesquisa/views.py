e9afd6afcc1f249c9ae592325c70aec632e23e5b
unknown <you@example.com>
2025-03-22 16:17:13 -0300

    quantidade por entrevistadora

--- pesquisa/views.py
+++ pesquisa/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
 from .models import Pesquisa
-from .forms import PesquisaForm  # Importe o formulário
+from .forms import PesquisaForm
+from django.db.models import Count
 
 def pesquisa_view(request):
+    total_registros = Pesquisa.objects.count()
+    registros_por_entrevistadora = Pesquisa.objects.values('colaboradora').annotate(total=Count('colaboradora')).order_by('colaboradora')
+
     if request.method == "POST":
-        form = PesquisaForm(request.POST)  # Preencha o formulário com os dados do POST
-        if form.is_valid():  # Verifique se o formulário é válido
-            form.save()  # Salve os dados no banco de dados
+        form = PesquisaForm(request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('pesquisa_concluida')
     else:
-        form = PesquisaForm()  # Crie um formulário vazio
+        form = PesquisaForm()
 
-    return render(request, 'pesquisa/formulario.html', {'form': form})
+    return render(request, 'pesquisa/formulario.html', {
+        'form': form,
+        'total_registros': total_registros,
+        'registros_por_entrevistadora': registros_por_entrevistadora
+    })
 
 def pesquisa_concluida_view(request):
     return render(request, 'pesquisa/concluido.html')
